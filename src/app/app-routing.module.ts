cba72aa62c7f8092f434e8fc263d941c9deb9467
mohamed351 <mohamed.perry351@gmail.com>
2022-07-03 21:26:14 +0200

    Add Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseComponent } from './layout/base/base.component';
 
 const routes: Routes = [
   { path:'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  {
+    path: '',
+    component: BaseComponent,
+    children:[
+      {
+        path: 'dashboard',
+        loadChildren: () => import("./dashboard/dashboard.module").then(m => m.DashboardModule)
+      },
+    ]
+  }
 ];
 
 @NgModule({
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { BaseComponent } from './base/base.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+import { NgbDropdownModule, NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
+
+
+@NgModule({
+  declarations: [BaseComponent, NavbarComponent, SidebarComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    NgbDropdownModule,
+    NgbCollapseModule,
+    PerfectScrollbarModule
+  ],
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }
+  ]
+})
+export class LayoutModule { }
