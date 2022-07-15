9fc1a03572129af19acecf411bb37025a455bc5c
mohamed351 <mohamed.perry351@gmail.com>
2022-07-15 21:59:05 +0200

    Add Product Serial

--- src/app/products/product-serials-list/product-serials-list.component.ts
+++ src/app/products/product-serials-list/product-serials-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewChecked, AfterViewInit, Component, OnChanges, OnInit, SimpleChanges, ViewChild, ViewChildren } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Serial } from 'src/app/model/serial';
 
 @Component({
   selector: 'app-product-serials-list',
@@ -7,13 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductSerialsListComponent implements OnInit {
 
-  constructor() { }
+@ViewChildren("#subSerials") subElements;
+   public currentList:Serial[] = []
+   public id:number;
+  constructor(private activeRouter:ActivatedRoute) {
+
+   }
+
+
+
 
   ngOnInit(): void {
+
+    this.activeRouter.params.subscribe(route =>{
+      this.id = route["id"];
+    });
   }
 
-  addMainSerail(value:String){
-    
+  addMainSerail(event,input:HTMLInputElement){
+    console.log(event);
+    if(event.key =="Enter"){
+
+      this.currentList.push({
+        productId:this.id,
+        serial : input.value,
+        subSerial:[]
+      })
+      input.value = "";
+        setInterval(()=>{
+          (document.getElementsByClassName("current-serial")[0] as HTMLInputElement).focus();
+        },100)
+
+    }
+
+
   }
+
+  deleteMainBarCode (serial:Serial){
+    this.currentList = this.currentList.filter(a=> a.serial !== serial.serial);
+  }
+  addSubSerial(event,serial:Serial, input:HTMLInputElement ){
+    if(event.key =="Enter"){
+      this.currentList.find(a=> a.serial ==  serial.serial).subSerial.push({
+        mainSerial:serial.serial,
+        serial:input.value
+      });
+      input.value ="";
+    }
+  }
+
+
+
+
 
 }
